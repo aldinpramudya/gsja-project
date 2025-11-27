ca47d4a27ce9c4ca806dcaa8eb921157705579e6
aldinpramudya <aldinarielpramudya@gmail.com>
2025-11-27 15:30:29 +0700

    feat(footer) : Footer with Mobile responsive

--- constants/index.tsx
+++ constants/index.tsx
@@ -27,13 +27,44 @@ export const navbarLinks = [
         ]
     },
     {
-        name : "Artikel",
-        href : "/artikel"
+        name: "Artikel",
+        href: "/artikel"
     },
     {
-        name : "Kontak Kami",
-        href : "/kontak-kami"
+        name: "Kontak Kami",
+        href: "/kontak-kami"
     },
 ]
 
 // Navbar Link Menu End
+
+// Footer Link Menu
+import { FaWhatsapp } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+export const footerLinks = [
+    {
+        title: "Tentang Kami",
+        links: [
+            { name: "Siapa Kami", href: "/tentang-kami" },
+            { name: "Gereja Kami", href: "/gereja-kami" },
+            { name: "Artikel", href: "/artikel" },
+            { name: "Susunan Panitia", href: "/susunan-panitia" }
+        ]
+    },
+    {
+        title: "Events",
+        links: [
+            { name: "Kalender Kegiatan", href: "/kalender-kegiatan" },
+        ]
+    },
+    {
+        title: "Kontak Kami",
+        links: [
+            { name: "WhatsApp", href: "https://wa.me/", icon: <FaWhatsapp /> },
+            { name: "Email", href: "mailto:/", icon: < MdEmail /> }
+        ]
+    }
+];
+
+// Footer Link Menu End
